3c94e3d4e2ff9d04c92f016c0801af9eea794227
Fadie Hannona <fadiehannona@gmail.com>
2014-12-16 19:10:46 +0000

    returns words when playing game

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -9,3 +9,9 @@ end
 def is_divisible_by_fifteen?(num)
 	num % 15 == 0
 end
+
+def fizzbuzz(num)
+	return "FizzBuzz" if is_divisible_by_fifteen?(num)
+	return "Fizz" if is_divisible_by_three?(num)
+	return "Buzz" if is_divisible_by_five?(num)
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -34,4 +34,20 @@ describe 'fizzbuzz' do
 
 	end
 
+	context 'while playing the game it returns' do
+
+		it "fizz" do
+			expect(fizzbuzz(3)).to eq("Fizz")
+		end
+
+		it "buzz" do
+			expect(fizzbuzz(5)).to eq("Buzz")
+		end
+
+		it "fizzbuzz" do
+			expect(fizzbuzz(15)).to eq("FizzBuzz")
+		end
+
+	end
+
 end
